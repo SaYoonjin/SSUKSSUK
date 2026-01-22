4b0ac05c7baa25e9e1bac8b09b02b57cc6f34751
DESKTOP-6OO0P6F\SSAFY <yangdahee0622@gmail.com>
2026-01-22 10:57:21 +0900

    chore: initialize BE package structure

---
+++ src/main/java/com/ssukssuk/ssukssuk/SsukssukApplication.java
@@ -0,0 +1,13 @@
+package com.ssukssuk.ssukssuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsukssukApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsukssukApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssukssuk/ssukssuk/SsukssukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssukssuk.ssukssuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsukssukApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
