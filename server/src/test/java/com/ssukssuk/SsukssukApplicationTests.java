d9cba4aa4ec9438d8fdc5e59fb7ed8fe9ebd64f9
DESKTOP-6OO0P6F\SSAFY <yangdahee0622@gmail.com>
2026-01-23 17:23:04 +0900

    Revert "fix: align Spring Boot plugin to 3.5.7"
    This reverts commit d1233e80a4cc0ffcaaa97e614ca9d9090754d1d4.

---
+++ server/src/test/java/com/ssukssuk/SsukssukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssukssuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsukssukApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
