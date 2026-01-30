6d72f74e7f8b7ced085525171da76de3ae021858
양다희 <yangdahee0622@gmail.com>
2026-01-30 10:08:46 +0900

    feat: plant image recent 14days repository query

--- server/src/main/java/com/ssukssuk/repository/history/PlantImageRepository.java
+++ server/src/main/java/com/ssukssuk/repository/history/PlantImageRepository.java
@@ -10,7 +10,22 @@ import java.util.List;
 
 public interface PlantImageRepository extends JpaRepository<PlantImage, Long> {
 
-
+    /**
+     * 특정 식물의 최근 기간 이미지 조회
+     * - from <= capturedAt < to
+     * - 최신순 정렬
+     */
+    @Query("""
+        select pi
+        from PlantImage pi
+        where pi.plant.plantId = :plantId
+          and pi.capturedAt >= :from
+          and pi.capturedAt < :to
+        order by pi.capturedAt desc
+    """)
+    List<PlantImage> findRecentImagesByPlantId(
+            @Param("plantId") Long plantId,
+            @Param("from") LocalDateTime from,
+            @Param("to") LocalDateTime to
+    );
 }
-
-
